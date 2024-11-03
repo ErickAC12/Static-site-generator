6bbe2661a3f03981faec6d08d6a8c3b4895eb9bc
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2024-11-02 20:58:45 -0600

    TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,38 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node, node2)
+
+    def test_nodeToHtml_text(self):
+        node = TextNode("This is only text", TextType.TEXT)
+        self.assertEqual(text_node_to_html_node(node).to_html(),
+                         'This is only text')
+
+    def test_nodeToHtml_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        self.assertEqual(text_node_to_html_node(node).to_html(),
+                         '<b>This is bold text</b>')
+
+    def test_nodeToHtml_italic(self):
+        node = TextNode("This is italic text", TextType.ITALIC)
+        self.assertEqual(text_node_to_html_node(node).to_html(),
+                         '<i>This is italic text</i>')
+
+    def test_nodeToHtml_code(self):
+        node = TextNode("This is code text", TextType.CODE)
+        self.assertEqual(text_node_to_html_node(node).to_html(),
+                         '<code>This is code text</code>')
+
+    def test_nodeToHtml_link(self):
+        node = TextNode("This is a link", TextType.LINK,
+                        "https://www.google.com")
+        self.assertEqual(text_node_to_html_node(node).to_html(),
+                         '<a href="https://www.google.com">This is a link</a>')
+
+    def test_nodeToHtml_image(self):
+        node = TextNode("This is an image", TextType.IMAGE,
+                        "../images/cool_pic.png")
+        self.assertEqual(text_node_to_html_node(node).to_html(),
+                         '<img src="../images/cool_pic.png" alt="This is an image"></img>')
 
 
 if __name__ == "__main__":
